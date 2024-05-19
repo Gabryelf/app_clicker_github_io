54dd6ec16b2bf2582fe42cb2332a53f00fe2cad5
Valeev Serj <145398532+Gabryelf@users.noreply.github.com>
2024-05-20 01:03:58 +0300

    Update main.py

--- main.py
+++ main.py
@@ -11,9 +11,9 @@ url_ref = "CookiesClickerGameBot"
 
 # Параметры подключения к базе данных MySQL
 db_config = {
-    'host': 'localhost',
-    'user': 'root',
-    'password': 'sksmel544332',
+    'host': 'https://portal.singlestore.com/organizations/7201eb28-5578-4258-bfd9-9acd5bc356f5/virtual-workspaces/580f4ded-ed69-4b92-a14d-3fbc6d33e083#access',
+    'user': 'serj_eleron',
+    'password': 'npPaWentMFadO9v5CXijZOl8zQvyqQYb',
     'database': 'app_clicker'
 }
 
