7b2c6ee8f1d1c0aee30c45af404fc26364ac2a4d
Valeev Serj <145398532+Gabryelf@users.noreply.github.com>
2024-05-20 15:16:26 +0300

    Update main.py

--- main.py
+++ main.py
@@ -10,33 +10,27 @@ client = MongoClient('mongodb+srv://eleronsmarya:l2kKlf3xMrWVadQ9@cluster1.lgx1g
 db = client['data_app']
 
 
-def get_or_create_user(user_id):
-    user_data = db.users.find_one({'user_id': user_id})
+def get_or_create_user(session_id):
+    user_data = db.users.find_one({'session_id': session_id})
     if not user_data:
-        # Создаем нового пользователя с уникальным ID
+        # Создаем нового пользователя с уникальным ID сессии
         user_data = {
-            'user_id': user_id,
+            'session_id': session_id,
             'name': None
         }
         db.users.insert_one(user_data)
     return user_data
 
-
-def save_user_name(user_id, name):
-    db.users.update_one({'user_id': user_id}, {'$set': {'name': name}})
-
+def save_user_name(session_id, name):
+    db.users.update_one({'session_id': session_id}, {'$set': {'name': name}})
 
 async def main(page: ft.Page):
     page.title = "MongoDB Flet App"
 
-    # Проверяем, есть ли у пользователя уникальный ID в localStorage
-    user_id = await page.local_storage.get("user_id")
-    if not user_id:
-        # Генерируем новый уникальный ID и сохраняем его в localStorage
-        user_id = str(ObjectId())
-        await page.local_storage.set("user_id", user_id)
+    # Инициализация переменной сессии
+    session_id = str(ObjectId())
 
-    user_data = get_or_create_user(user_id)
+    user_data = get_or_create_user(session_id)
 
     if user_data['name']:
         # Если имя пользователя уже сохранено, показываем приветствие
@@ -45,21 +39,19 @@ async def main(page: ft.Page):
     else:
         # Если имя пользователя не сохранено, запрашиваем его
         name_input = ft.TextField(label="Enter your name", autofocus=True)
-        enter_button = ft.TextButton(text="Enter", on_click=lambda e: enter_click(e, name_input, user_id, page))
+        enter_button = ft.TextButton(text="Enter", on_click=lambda e: enter_click(e, name_input, session_id, page))
         page.add(name_input, enter_button)
 
     await page.update()
 
-
-async def enter_click(event, name_input, user_id, page):
+async def enter_click(event, name_input, session_id, page):
     # Сохраняем имя пользователя в базе данных
-    save_user_name(user_id, name_input.value)
+    save_user_name(session_id, name_input.value)
     # Обновляем страницу, показываем приветствие
     page.controls.clear()
     greeting = ft.Text(f"Welcome, {name_input.value}!")
     page.add(greeting)
     await page.update()
 
-
 if __name__ == "__main__":
-    ft.app(target=main)
+    ft.app(target=main, view=ft.WEB_BROWSER)
